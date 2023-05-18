34f6744dd0fa506e08a8bfd4958cc6da42ce8591
achidester <chidesta@oregonstate.edu>
2023-05-18 15:26:16 -0700

    Removed option to choose more expensive bot

--- webapp/adam-gpt-streamlit.py
+++ webapp/adam-gpt-streamlit.py
@@ -30,34 +30,12 @@ if 'total_tokens' not in st.session_state:
 if 'total_cost' not in st.session_state:
     st.session_state['total_cost'] = 0.0
 
-# Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
-st.sidebar.title("Sidebar")
-model_name = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
+
+model = "gpt-3.5-turbo"
+model_name = "GPT-3.5"
+
 counter_placeholder = st.sidebar.empty()
 counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
-clear_button = st.sidebar.button("Clear Conversation", key="clear")
-
-
-# Map model names to OpenAI model IDs
-if model_name == "GPT-3.5":
-    model = "gpt-3.5-turbo"
-else:
-    model = "gpt-4"
-
-# reset everything
-if clear_button:
-    st.session_state['generated'] = []
-    st.session_state['past'] = []
-    st.session_state['messages'] = [
-        {"role": "system", "content": "You are a helpful assistant."}
-    ]
-    st.session_state['number_tokens'] = []
-    st.session_state['model_name'] = []
-    st.session_state['cost'] = []
-    st.session_state['total_cost'] = 0.0
-    st.session_state['total_tokens'] = []
-    counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
-
 
 # generate a response
 def generate_response(prompt):
@@ -83,7 +61,7 @@ response_container = st.container()
 container = st.container()
 
 
-#GETTING AND PRINTING COST METRICS FOR USING OPENAI
+#GETTING AND PRINTING COST METRICS FOR USING OPENAI (CAN USE TO DISALLOW OVER USERS ACCESS)
 with container:
     with st.form(key='my_form', clear_on_submit=True):
         user_input = st.text_area("You:", key='input', height=100)
