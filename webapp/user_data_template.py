a8516bf890ca30ab9b02a51888ab3de948c93e31
orion-junkins <orionjunkins@gmail.com>
2023-06-01 14:51:03 -0700

    user data file moved

---
+++ webapp/user_data_template.py
@@ -0,0 +1,14 @@
+user_data_template = {
+    "business_name": {
+        "Context": "Ask me the name of the business I want to start. Continue to ask me until I tell you. Once you know the name of the business I want to start, reply with a single message containing nothing other than four pound signs, the name of the business, and then another four pound signs (####[some business name]####).)",
+        "Response": None
+    },
+    "business_tagline": {
+        "Context": "Ask me what my tagline is for the business. Continue to ask me until I tell you. Once you know what my tagline is for the business, reply with a single message containing nothing other than four pound signs, my tagline for the business, and then another four pound signs (####[some tagline]####).)",
+        "Response": None
+    },
+    "business_structure": {
+        "Context": "Ask me question to find out what kind of business I should start. ie should I form an LLC, an S Corp, C Corp, a Nonprofit, etc. Encourage me to share details about the product or service I want to offer until you know enough to choose the best option. Once you know what business I want to start, reply with a single message containing nothing other than four pound signs, the business type, and then another four pound signs (####[some business idea]####).)",
+        "Response": None
+    }    
+}
