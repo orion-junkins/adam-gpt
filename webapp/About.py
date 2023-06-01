a3c6f28c4bb2a6ffe0706f95b6dcb824ca212bfd
achidester <chidesta@oregonstate.edu>
2023-06-01 13:51:47 -0700

    edits to about page

--- webapp/About.py
+++ webapp/About.py
@@ -12,10 +12,12 @@ st.markdown(
     Adam-GPT is an OSU Cascades CS Business of Software (SE467) student project led by Adam Krynicki. The projects goal is to provide support to entrepreneur in getting their startup or small business off the ground.
     Adam-GPT is a virtual business mentor in the form of a generative ai chatbot. Adam-GPT provides business related advice with links to industry specific documentation. 
     ### How-to: 
+    Adam-GPT will help guide you through the process.
+    <- Click on "Adam-GPT" in the sidebar to get started 
+
 
     ### The Team
     - Adam Krynicki
-        - Lawyer Business-expert
     - Alex Chidester
     - Caleb Knight
     - Kimberly Markley
